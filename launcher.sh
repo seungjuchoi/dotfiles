c00fe59b37475cc1089a2443378806a0cbc66cf6
seungju24.choi <seungju24.choi@lge.com>
2022-01-17 16:36:06 +0900

    neovim and vim plug

--- launcher.sh
+++ launcher.sh
@@ -1,24 +1,11 @@
-### Update vim pakcage
+### Update Pakcage
 sudo apt update
-sudo apt install vim
-sudo apt install exuberant-ctags
-
-### Install Vundle package
-VUNDLE_PATH="$HOME/.vim/bundle/Vundle.vim"
-if [ ! -d "$VUNDLE_PATH" ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_PATH
-fi
+sudo apt upgrade
+sudo apt install neovim git curl nodejs npm exuberant-ctags cmake
 
 ### Copy rc files
-cp .vimrc ~
-cp .screenrc ~
-
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/
 
 ### Install vim plugins
-vim +PluginInstall
-
-
-### Git setting
-git config --global user.email "redreamer@gmail.com"
-git config --global user.name "seungjuchoi"
-git config --global push.default matching
+nvim +PlugInstall
