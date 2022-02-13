91e5d9b1b4806d5bbded3cfd04e5ba564db589a2
seungjuchoi <seungjuchoi@lge.com>
2022-02-13 19:38:01 +0900

    Remove some sh files

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-### Update Pakcage
-sudo apt update
-sudo apt upgrade
-sudo apt install neovim git curl nodejs npm exuberant-ctags cmake python-neovim python3-neovim llvm fd -y
-
-### Copy rc files
-mkdir -p ~/.config/nvim
-cp init.vim ~/.config/nvim/
-
