772d62172d7e3451ad07cc5044a075ce8cfe3e2c
seungju24.choi <seungju24.choi@lge.com>
2022-09-07 23:19:15 +0900

    convert function to alias

--- config.fish
+++ config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting ""
-    alias vi nvim
-    function ll
-        command exa --group-directories-first -l --icons
+    command -qs nvim && alias vi nvim
+    if command -qs exa
+        alias ll "exa --group-directories-first -l --icons"
+        alias lla "exa --group-directories-first -l --icons -a"
     end
-    function lla
-        command exa --group-directories-first -l --icons -a
+    if command -qs lazygit
+        alias lg lazygit
     end
-
+    alias min "open -a min"
     set -gx ICLOUD_PATH /Users/(whoami)/Library/Mobile\ Documents/com~apple~CloudDocs
 end
-
