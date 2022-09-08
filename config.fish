3f471b510f8056580d591e3c0cdbf5d969672caf
seungjuchoi <redreamer@gmail.com>
2022-09-09 00:05:42 +0900

    argv for alias

--- config.fish
+++ config.fish
@@ -3,11 +3,11 @@ if status is-interactive
     set fish_greeting ""
     command -qs nvim && alias vi nvim
     if command -qs exa
-        alias ll "exa --group-directories-first -l --icons"
-        alias lla "exa --group-directories-first -l --icons -a"
+        alias ll "exa --group-directories-first -l --icons $argv"
+        alias lla "exa --group-directories-first -l --icons -a $argv"
     end
     if command -qs lazygit
-        alias lg lazygit
+        alias lg lazygit $argv
     end
     alias min "open -a min"
     set -gx ICLOUD_PATH /Users/(whoami)/Library/Mobile\ Documents/com~apple~CloudDocs
