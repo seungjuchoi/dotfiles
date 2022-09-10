f4b7e49030741cff2a1c095c7ff2ba867e4ee27e
seungjuchoi <redreamer@gmail.com>
2022-09-11 01:39:53 +0900

    add initvim in setup.fish

--- setup.fish
+++ setup.fish
@@ -1,4 +1,6 @@
 #!/usr/local/bin/fish
+command sudo ln -s -f $PWD/init.vim ~/.config/nvim/init.vim
+echo init.vim loaded
 command sudo ln -s -f $PWD/tmux.conf ~/.tmux.conf 
 echo tmux.conf loaded
 command sudo ln -s -f $PWD/tmux.statusline.conf ~/.tmux.statusline.conf 
