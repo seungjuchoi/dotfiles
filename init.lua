456e1367b45cadb9c20fe9fcda23ec0a22117d72
seungju24.choi <seungju24.choi@lge.com>
2022-09-19 23:31:37 +0900

    update os luafiles

--- init.lua
+++ init.lua
@@ -2,8 +2,20 @@ require("seungju.base")
 require("seungju.plugins")
 require("seungju.keymap")
 
-if vim.fn.has("macunix") == 1 then
-    require("seungju.macos")
-else
-    require("seungju.windows")
+-- if vim.fn.has("macunix") == 1 then
+--     require("seungju.macos")
+-- else
+--     require("seungju.windows")
+-- end
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('seungju.macos')
+end
+if is_win then
+  require('seungju.windows')
 end
