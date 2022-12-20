169060c88b0437b83774f94c2f1677e54b04a4cb
seungjuchoi <redreamer@gmail.com>
2022-12-21 01:43:32 +0900

    add wezterm rc

---
+++ wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font_with_fallback {
+    'JetBrains Mono',
+    {family = 'Monoplex KR', weight = 'Medium'},
+    'Apple SD Gothic Neo',
+    -- 'Hack Nerd Font',
+    'Fira Code',
+    'DengXian',
+  },
+  font_size = 13,
+  use_ime = true,
+  keys = {
+    {
+      key = 'Enter',
+      mods = 'ALT',
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+  },
+  color_scheme = "OceanicMaterial",
+  -- hyperlink_rules = {
+  --   {
+  --     regex = '\\b[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+  --     format = '$0',
+  --   }
+  -- }
+}
+
+
