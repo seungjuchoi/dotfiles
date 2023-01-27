33aec24d732e8c4fa64c91bf1e0d507aa45c2d2e
seungjuchoi <redreamer@gmail.com>
2023-01-28 01:06:29 +0900

    formating with section by f key

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -14,9 +14,16 @@ local on_attach = function(_, bufnr)
     end
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
+  local vmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+    vim.keymap.set('v', keys, func, { buffer = bufnr, desc = desc })
+  end
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  vmap('f', vim.lsp.buf.format, '[F]ormat by Selection')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
