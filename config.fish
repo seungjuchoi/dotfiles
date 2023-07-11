b050d1ec787c4ff4017512d6bcc2b2270eb6f407
seungjuchoi <redreamer@gmail.com>
2023-07-12 01:29:07 +0900

    remove somethign in interactive shell

--- config.fish
+++ config.fish
@@ -1,35 +1,32 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting ""
-    if command -qs nvim
-        alias vi nvim
-        alias vim nvim
-        set -gx EDITOR nvim
-    end
-    if command -qs ipython
-        alias ipy ipython
-    end
-    if command -qs exa
-        alias ll "exa --group-directories-first -l --icons $argv"
-        alias lla "exa --group-directories-first -l --icons -a $argv"
-    end
-    if command -qs lazygit
-        alias lg lazygit $argv
-    end
-    if command -qs convmv
-        alias convmv "convmv -r -f utf8 -t utf8 --notest --nfc $argv"
-    end
-    if command -qs starship
-        starship init fish | source
-    end
-    set -gx ICLOUD_PATH /Users/(whoami)/Library/Mobile\ Documents/com~apple~CloudDocs
-    if test -d /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
-        set -gx LIBRARY_PATH "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
-    end
-    set LOCAL_CONFIG (dirname (status --current-filename))/config_local.fish
-    if test -f $LOCAL_CONFIG
-        source $LOCAL_CONFIG
-    end
+set fish_greeting ""
+if command -qs nvim
+    alias vi nvim
+    alias vim nvim
+    set -gx EDITOR nvim
+end
+if command -qs ipython
+    alias ipy ipython
+end
+if command -qs exa
+    alias ll "exa --group-directories-first -l --icons $argv"
+    alias lla "exa --group-directories-first -l --icons -a $argv"
+end
+if command -qs lazygit
+    alias lg lazygit $argv
+end
+if command -qs convmv
+    alias convmv "convmv -r -f utf8 -t utf8 --notest --nfc $argv"
+end
+if command -qs starship
+    starship init fish | source
+end
+set -gx ICLOUD_PATH /Users/(whoami)/Library/Mobile\ Documents/com~apple~CloudDocs
+if test -d /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
+    set -gx LIBRARY_PATH "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+end
+set LOCAL_FISH_CONF (dirname (status --current-filename))/config_local.fish
+if test -f $LOCAL_FISH_CONF
+    source $LOCAL_FISH_CONF
 end
 if test -d /opt/homebrew/opt/llvm
     set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
@@ -43,10 +40,6 @@ if test -d /opt/homebrew/opt/opencv@3
    set -gx LDFLAGS $LDFLAGS "-L/opt/homebrew/opt/opencv@3/lib"
    set -gx CPPFLAGS $CPPFLAGS "-I/opt/homebrew/opt/opencv@3/include"
 end
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
 
 # python
 set -gx PYTHONBREAKPOINT "ipdb.set_trace"
