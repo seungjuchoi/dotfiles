40c58d3c5d2999cc52af8ee7cbe99b8aa9677575
seungjuchoi <redreamer@gmail.com>
2023-07-16 18:38:25 +0900

    add run command for python, add copilot

--- lua/seungju/plugins.lua
+++ lua/seungju/plugins.lua
@@ -165,5 +165,21 @@ require('lazy').setup({
             "<cmd>UndotreeToggle<CR>"
             )
         end
+    },
+    'github/copilot.vim',
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        config = function()
+            local home = vim.fn.expand("$HOME")
+            require("chatgpt").setup({
+                api_key_cmd = "gpg --decrypt " .. home .. "/openai_api_key.gpg"
+            })
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
     }
 })
