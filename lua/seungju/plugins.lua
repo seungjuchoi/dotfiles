71d2d187e30bbd7336d30731e691d0c2b96ced90
seungju24.choi <seungju24.choi@lge.com>
2023-08-06 20:46:14 +0900

    remove nvim-tree playground

--- lua/seungju/plugins.lua
+++ lua/seungju/plugins.lua
@@ -81,7 +81,6 @@ require('lazy').setup({
             pcall(require('nvim-treesitter.install').update { with_sync = true })
         end,
     },
-    'nvim-treesitter/playground',
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
         dependencies = 'nvim-treesitter',
