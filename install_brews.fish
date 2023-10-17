68ce6e189ce0a30b583745510cc0baceb6a08cc6
seungjuchoi <redreamer@gmail.com>
2023-10-17 18:50:23 +0900

    add fisher plugin

--- install_brews.fish
+++ install_brews.fish
@@ -1,6 +1,7 @@
 # WARN: Install Fish and Brew before execution
 
-set -l packages zoxide \
+set -l packages \
+        zoxide \
         fd \
         ripgrep \
         nvim \
@@ -10,15 +11,35 @@ set -l packages zoxide \
         yazi \
         exa \
         ranger \
+        fisher \
+        rust \
+        lua \
+        luarocks \
+        llvm \
+        neofetch \
+        bat \
+        mdcat \
+        eza \
+        lazygit \
+
 
 brew install $packages
 brew update
 brew upgrade
 
 pipx install virtualfish
+vf install
 
 #tmux
-if test -d ~/.tmux/plugins/tpm
+# INFO: Enter tmux and then Hit Ctrl+t, I to install tmux-packges
+if not test -d ~/.tmux/plugins/tpm
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 end
-# INFO: Enter tmux and then Hit Ctrl+t, I to install tmux-packges
+
+#fisher
+set -l packages \
+        jorgebucaran/autopair.fish \
+        PatrickF1/fzf.fish \
+        jorgebucaran/nvm.fish \
+
+fisher install $packages
