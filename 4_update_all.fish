81b292b32b22c2372092db2568e9a443c673c71f
seungju24.choi <seungju24.choi@lge.com>
2024-03-29 16:52:29 +0900

    update fishes

--- 4_update_all.fish
+++ 4_update_all.fish
@@ -1,3 +1,21 @@
+#!/usr/bin/env fish
 command brew update; brew upgrade
 fisher update
 command ~/.tmux/plugins/tpm/bin/update_plugins all
+
+set current_folder (pwd)
+set yazi_plugins ~/.config/yazi/plugins
+if test -d $yazi_plugins/glow.yazi
+    cd $yazi_plugins/glow.yazi
+    git pull
+end
+if test -d $yazi_plugins/miller_csv.yazi
+    cd $yazi_plugins/miller_csv.yazi
+    git pull
+end
+if test -d $yazi_plugins/hexyl.yazi
+    cd $yazi_plugins/hexyl.yazi
+    git pull
+end
+
+cd $current_folder
