35051e41da522a5e1e9c84841f4a90460028dd71
seungju24.choi <seungju24.choi@lge.com>
2024-03-29 17:48:05 +0900

    add 0_pre pack

--- 0_pre_in_docker.sh
+++ 0_pre_in_docker.sh
@@ -1,9 +1,8 @@
 apt update
-apt install git curl python3-pip
+apt install -y git curl python3-pip build-essential
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 brew install fish
 echo $(which fish) > tee -s /etc/shells
 chsh -s $(which fish)
 exec fish -C "git clone http://github.com/seungjuchoi/dotfiles;cd dotfiles;fish_add_path /home/linuxbrew/.linuxbrew/bin;fish_add_path ~/.local/bin"
-
