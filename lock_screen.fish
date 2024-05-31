39d292d4b41d072d036cc2a7f51ac7bc6efb51b6
seungjuchoi <redreamer@gmail.com>
2024-05-31 03:44:17 +0000

    update lock_screen script

--- lock_screen.fish
+++ lock_screen.fish
@@ -1,5 +1,14 @@
 #!/usr/bin/env fish
 
+if set -q DISPLAY
+        set display_orig $DISPLAY
+        set display_was_set true
+else
+        set display_was_set false
+end
+
+set -x DISPLAY :1
+
 if command -qs i3lock
         command i3lock &
 else
@@ -10,3 +19,11 @@ end
 command sleep 1
 
 command xset dpms force off
+
+if test $display_was_set = true
+        set -x DISPLAY $display_orig
+else
+        set -e DISPLAY
+end
+
+set -e display_was_set display_orig
