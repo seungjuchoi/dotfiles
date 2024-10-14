b6756da2cabd4cb5ae1470b8b3c66a3f975bacfc
seungjuchoi <redreamer@gmail.com>
2024-10-14 22:38:48 +0900

    add avante

--- lua/seungju/plugins.lua
+++ lua/seungju/plugins.lua
@@ -157,7 +157,7 @@ require("lazy").setup({
           ["=="] = "!=",
         },
       })
-      vim.keymap.set("n", "<leader>ta", "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>")
+      vim.keymap.set("n", "<leader>tt", "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>")
     end,
     event = { "BufReadPost" },
   },
@@ -349,5 +349,50 @@ require("lazy").setup({
         require("codeium").setup({
         })
     end
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "openai"
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   },
 })
