a1da7e4542290188e50ba23dddfb5dc6dbd35c4b
seungju24.choi <seungju24.choi@lge.com>
2024-10-16 19:09:13 +0900

    update prx script

--- prx.fish
+++ prx.fish
@@ -1,11 +1,18 @@
 function prx
     if test (count $argv) -eq 1
-        set port $argv[1]
-        set -gx http_proxy socks5h://127.0.0.1:$port
-        set -gx https_proxy socks5h://127.0.0.1:$port
-        set -gx no_proxy 127.0.0.1,localhost
-        echo "Proxy set to 127.0.0.1:$port"
+        if test $argv[1] = "off"
+            set -e http_proxy
+            set -e https_proxy
+            set -e no_proxy
+            echo "Proxy settings have been removed."
+        else
+            set port $argv[1]
+            set -gx http_proxy socks5h://127.0.0.1:$port
+            set -gx https_proxy socks5h://127.0.0.1:$port
+            set -gx no_proxy 127.0.0.1,localhost
+            echo "Proxy set to 127.0.0.1:$port"
+        end
     else
-        echo "Usage: prx <port>"
+        echo "Usage: prx <port> or prx off"
     end
 end
