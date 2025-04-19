a27dc7f72c76ff8e7c600b18a449d7fd4c60fff8
seungjuchoi <redreamer@gmail.com>
2025-04-19 22:50:04 +0900

    remove redundant code

--- lua/seungju/plugins.lua
+++ lua/seungju/plugins.lua
@@ -323,23 +323,6 @@ require("lazy").setup({
     end,
   },
   {
-    'kristijanhusak/vim-dadbod-ui',
-    dependencies = {
-      { 'tpope/vim-dadbod', lazy = true },
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
-    },
-    cmd = {
-      'DBUI',
-      'DBUIToggle',
-      'DBUIAddConnection',
-      'DBUIFindBuffer',
-    },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
-    end,
-  },
-  {
     "Exafunction/codeium.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
