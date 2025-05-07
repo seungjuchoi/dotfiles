60e606d872ce36eca5d34031d60b06a1bbb33d4a
seungjuchoi <redreamer@gmail.com>
2025-05-07 23:13:57 +0900

    apply mason lspconfig v2.0

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -76,17 +76,6 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
-  automatic_installation = true,
-})
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    })
-  end,
 })
 
 mason_tool_installer.setup({
