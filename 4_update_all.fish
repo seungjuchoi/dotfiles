b1fa7835c043a190233538defabe3d9c92c3dfb6
seungju24.choi <seungju24.choi@lge.com>
2026-01-13 13:15:05 +0900

    refactor: improve update script robustness

--- 4_update_all.fish
+++ 4_update_all.fish
@@ -1,14 +1,20 @@
 #!/usr/bin/env fish
 if type -q brew
-    command brew update; brew upgrade
+    command brew update; and brew upgrade
 end
-fisher update
+
+if type -q fisher
+    fisher update
+end
+
 if type -q tmux
     command ~/.tmux/plugins/tpm/bin/update_plugins all
 end
+
 if type -q ya
     command ya pkg upgrade
 end
+
 if type -q nvim
     nvim --headless "+Lazy! sync" +qa
 end
